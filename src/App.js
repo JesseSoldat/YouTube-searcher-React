f3d47598675decd6df339a665db49faf5b23d2ef
jesse <jsoldat@hotmail.com>
2017-06-20 17:02:32 -0400

    can search for videos on youtube

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyAqATbr65c9EON9JW6nSf5yEiHK-mtpdPg';
 
-const ApiKey = 'AIzaSyAqATbr65c9EON9JW6nSf5yEiHK-mtpdPg';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('attack on titan');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
+      console.log(videos);
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar onSearchTermChanged={this.videoSearch} />
+       
       </div>
     );
   }
